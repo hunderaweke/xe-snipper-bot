d3a8e09c157c56768c4212012ae44aa5e55bf82b
Hundera Awoke <hunderaweke@gmail.com>
2024-04-27 20:56:53 +0300

    changed: plan order

--- core/config.py
+++ core/config.py
@@ -4,4 +4,4 @@ from typing import Final
 
 TOKEN = config("TOKEN")
 
-USDT_ADDRESS: Final[str] = "6661370496:AAENCRyfkghI1spqh0SASjmWMg9oRTjkRho"
+USDT_ADDRESS: Final[str] = "TNsZjKUckDBLTcojMtab3i3724Ndup3yxK"
